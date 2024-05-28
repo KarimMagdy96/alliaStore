235b0aadf72487ce9cfa8a0deea9ec7569f2f762
Karim <karimmagdy096@gmail.com>
2024-05-28 15:26:35 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { Footer } from "./components/Footer/Footer";
 import { Home } from "./components/Home/Home";
 import { Layout } from "./components/Layout/Layout";
 import { Navbar } from "./components/Navbar/Navbar";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  HashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import { ErrorPage } from "./components/Error/ErrorPage";
 import { Classic } from "./components/Classic/Classic";
 import { Casual } from "./components/Casual/Casual";
 
 function App() {
-  const Routing = createBrowserRouter([
+  const Routing = createHashRouter([
     {
       path: "/",
       element: <Layout />,
--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./error.css";
+import { Link } from "react-router-dom";
 
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <section>
+      <div>
+        <section className="error-container">
+          <span className="four">
+            <span className="screen-reader-text">4</span>
+          </span>
+          <span className="zero">
+            <span className="screen-reader-text">0</span>
+          </span>
+          <span className="four">
+            <span className="screen-reader-text">4</span>
+          </span>
+        </section>
+        <div className="link-container">
+          <Link to="/home" className="more-link">
+            Back To Home
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
